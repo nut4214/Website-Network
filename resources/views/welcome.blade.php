388b6577f48dc314b88b4d6cd293c09e2264ee95
nut4214 <nut4214@hotmail.com>
2018-11-24 23:32:52 +0700

    edit link text in welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
                 </div>
 
                 <div class="links">
-                    <a href="{{url('/test')}}">Text</a>
+                    <a href="{{url('/text')}}">Text</a>
                     <a href="{{url('/video')}}">Video</a>
                     <a href="{{url('/picture')}}">Picture</a>
 
