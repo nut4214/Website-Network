141492d6cb2d6df61b9135adfc58527ec54ce50c
nut4214 <nut4214@hotmail.com>
2018-11-25 01:16:48 +0700

    edit header

--- resources/views/Text.blade.php
+++ resources/views/Text.blade.php
@@ -6,7 +6,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Picture</div>
+                <div class="card-header">Text</div>
 
                 <div class="card-body">
                     @if (session('status'))
