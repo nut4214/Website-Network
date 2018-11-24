2786ef01d51ed6cd66e0879ec1971c2bc149228f
nut4214 <nut4214@hotmail.com>
2018-11-24 11:03:01 +0700

    Edit layout video view

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -1,4 +1,19 @@
 <!DOCTYPE html>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 <html>
 <head>
 	<meta charset="utf-8" />
@@ -16,3 +31,9 @@
 </video>
 </body>
 </html>
+</div>
+</div>
+</div>
+</div>
+</div>
+@endsection
