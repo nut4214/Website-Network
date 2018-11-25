773981cfb181209a8c067d64f87b191c0f9af30c
paxchatphimuk <45060077+paxchatphimuk@users.noreply.github.com>
2018-11-25 22:12:05 +0700

    edit file name picture.blade.php to network picture by pax

--- resources/views/picture.blade.php
+++ resources/views/picture.blade.php
@@ -21,24 +21,24 @@
   <h1><center><font color="blue" size="15"><I><B><U>Picture Page</U></B></I></font></center></h1>
 
         <table border="5" bordercolor="green" width="50%">
-            <tr><td bgcolor="#ffffe6"><font color="green" size="5"><center><B>MARIO</B></center></font></td>
-            <td><center><img src="images/PNG.png" width="300" height="300"></center></td></tr>
+            <tr><td bgcolor="#ffffe6"><font color="green" size="5"><center><B>Token-ring</B></center></font></td>
+            <td><center><img src="images/PNG1.png" width="300" height="300"></center></td></tr>
             <tr><td width="100" height="100"><center><B><font color="green" size="5">รูปประเภท.png</font></B></center></td><td width="300" height="300">
-            <a href="https://vignette.wikia.nocookie.net/fantendo/images/6/6e/Small-mario.png/revision/latest?cb=20120718024112"><center>ไปที่ลิงค์รูปภาพต้นฉบับ</center></a></td></tr>
+            <a href="https://s.hswstatic.com/gif/ethernet4.gif"><center>ไปที่ลิงค์รูปภาพต้นฉบับ</center></a></td></tr>
         </table>
 
         <table border="5" bordercolor="red" width="50%">
-            <tr><td bgcolor="#ffffe6"><font color="red" size="5"><center><B>MICKEY</B></center></font></td>
-            <td><center><img src="images/jpeg.jpg" width="300" height="300"></center></td></tr>
+            <tr><td bgcolor="#ffffe6"><font color="red" size="5"><center><B>OSI Model</B></center></font></td>
+            <td><center><img src="images/jpeg1.jpg" width="300" height="300"></center></td></tr>
             <tr><td width="100" height="100"><center><B><font color="red" size="5">รูปประเภท.jpg</font></B></center></td><td width="300" height="300">
-            <a href="https://vignette.wikia.nocookie.net/cartoons/images/8/86/Mickey-mouse-10.jpg.jpeg/revision/latest?cb=20090310014517"><center>ไปที่ลิงค์รูปภาพต้นฉบับ</center></a></td></tr>
+            <a href="https://cdn-images-1.medium.com/max/1600/0*vNEkf-2ppecgkZCb.jpg"><center>ไปที่ลิงค์รูปภาพต้นฉบับ</center></a></td></tr>
         </table>
 
          <table border="5" bordercolor="orange" width="50%">
-            <tr><td bgcolor="#ffffe6"><font color="orange" size="5"><center><B>CAT</B></center></font></td>
-            <td><center><img src="images/giphy.gif" width="300" height="300"></center></td></tr>
+            <tr><td bgcolor="#ffffe6"><font color="orange" size="5"><center><B>Network Topology</B></center></font></td>
+            <td><center><img src="images/giphy1.gif" width="300" height="300"></center></td></tr>
             <tr><td width="100" height="100"><center><B><font color="orange" size="5">รูปประเภท.gif</font></B></center></td><td width="300" height="300">
-            <a href="https://media2.giphy.com/media/iz4XhMigHIat2/giphy.gif?cid=3640f6095bf2d0a74861366f51d24c73"><center>ไปที่ลิงค์รูปภาพต้นฉบับ</center></a></td></tr>
+            <a href="https://giphy.com/gifs/9PcQDGVja38Eseg8pi"><center>ไปที่ลิงค์รูปภาพต้นฉบับ</center></a></td></tr>
         </table>
 
   </body>
