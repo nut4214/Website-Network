7fabfba214e3538dab160e98903cc0f6e6b7285a
paxchatphimuk <45060077+paxchatphimuk@users.noreply.github.com>
2018-11-25 22:34:30 +0700

    add file name Pax

--- resources/views/Text.blade.php
+++ resources/views/Text.blade.php
@@ -1,53 +1,23 @@
 <!DOCTYPE html>
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Text</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 <html>
 <head>
-
+	
 </head>
 <body>
 	<h1><center><font color="blue" size="15"><I><B><U>Text Page</U></B></I></font></center></h1>
 
-		<table border="5" width="50%">
-			<tr><td style="font-family:verdana">This is font</td>
-			<td style="font-family:courier;">รูป<a href="https://f.ptcdn.info/754/014/000/1390317148-xs-o.jpg">แมว</a>อ่ะอยากดูป่ะ</td>
-			<p style="font-family:Times New Roman; font-style: italic;"><header><marquee behavior="slide"><b>แพทยสภา เตรียมเรียกหมอสูติฯฉาวสอบจริยธรรม</b></marquee></header>แพทยสภา ระบุสูตินรีแพทย์ที่ถูกกล่าวหาข่มขืนคนไข้ยังบริสุทธิ์สามารถตรวจคนไข้ได้ตามปกติ เตรียมเรียกสอบจริยธรรมธ.ค.นี้
-
-วันนี้( 23 พ.ย.61) นายอัจฉริยะ เรืองรัตนพงษ์ ประธานชมรมช่วยเหลือเหยื่ออาชญากรรม นำหลักฐานการตรวจสอบคลีนิคในจังหวัดนครสวรรค์ พร้อมภาพวีดีโอจำลองเหตุการณ์ เข้าร้องเรียนต่อแพทยสภา ให้เอาผิดด้านจริยธรรมสูตินรีแพทย์ ที่ถูกกล่าวหาว่าล่วงละเมิดทางเพศคนไข้สาวจำนวนมากระหว่างการตรวจรักษา</p></tr>
-			<tr><td style="font-family:Georgia; font-style: normal;" >This is font</td>
-			<td style="font-family:Arial; font-style: oblique;">This is font 2</td>
-			<td style="font-family:Lucida Console;">This is font 3</td></tr>
-		</table>
-
-		<table border="5" width="50%">
-			<tr><td><marquee direction="left">ก็อปมาไม่ต้องถามว่าทำไง</marquee></td>
-			<td><marquee direction="down">ก็อปมาไม่ต้องถามว่าทำไง</marquee></td>
-			<td><marquee behavior="alternate" scrollamount="1">นี่ก็ก็อปมาอย่าเสือกถาม<marquee></td></tr>
-			<tr><td><a href="https://www.w3schools.com/html/html_links.asp">อยากขยายยยยยยยยยยยยย</a></td>
-			<td><a href="https://www.w3schools.com/html/tryit.asp?filename=tryhtml_styles_font-family">ให้เห็นว่าาาาาาาาาา</a></td>
-			<td><a href="https://www.w3schools.com/html/tryit.asp?filename=tryhtml_links">Text มันวิ่งด๊ายยยยยยยย</a></td></tr>
-		</table>
+		
+			<p style="font-family:Times New Roman; font-style: italic; text-indent: 50px"><header><marquee behavior="slide" scrollamount="30"><b>HTTP HTTPS คืออะไร โพรโทคอล แลกเปลี่ยนข้อมูล</b></marquee></header>HTTP ย่อมาจาก Hypertext Transfer Protocol คือ โพรโทคอลสื่อสารสำหรับการแลกเปลี่ยนสารสนเทศผ่านอินเทอร์เน็ต โดยหลักแล้วใช้ในการรับเอกสารข้อความหลายมิติที่นำไปสู่การเชื่อมต่อกับ World Wide Web(WWW )จะใช้เมื่อเรียกโปรแกรมweb browser เช่น Firefox, Google Chrome, Safari,Opera และ IE Microsoft Internet Explorer เรียกดูข้อมูลหรือเว็บเพจ โปรแกรมบราวเซอร์ดังกล่าวจะใช้โปรโตคอล HTTP ซึ่งโปรโตคอลนี้ทำให้เซิร์ฟเวอร์ส่งข้อมูลมาให้บราวเซอร์ตามต้องการ และบราวเซอร์จะนำข้อมูลมาแสดงผลบนจอภาพได้อย่างถูกต้อง
+ในการแลกเปลี่ยนข้อมูลกันระหว่าง Server และ Client ของ World Wide Web (Server) โดยส่งข้อมูลแบบ Clear text คือ ข้อมูลที่ทำการส่งไปนั้น ไม่ได้ทำการเข้ารหัส ทำให้สามารถถูกดักจับและอ่านข้อมูลได้ง่าย</p>
+<p style="font-family:Times New Roman; font-style: italic; text-indent: 50px"><header><marquee behavior="slide" scrollamount="30">HTTPS คืออะไร</marquee></header></p>
+<pstyle="font-family:Times New Roman; font-style: italic; text-indent: 50px">https ย่อมาจาก Hypertext Transfer Protocol over Secure Socket Layer หรือ http over ssl คือ โปรโตคอลที่ระบุถึงการเชื่อมต่อแบบ Secure http โปรโตคอล https สร้างเพื่อความปลอดภัย
+ในการสื่อสารผ่านอินเตอร์เน็ตข้อมูลที่ทำการส่งได้ถูกเข้ารหัสเอาไว้ โดยใช้ Asymmetric Algorithm ซึ่งถ้าถูกดักจับได้ก็ไม่สามารถที่จะอ่านข้อมูลนั้นได้รู้เรื่อง โดยข้อมูลนั้นจะสามารถอ่านได้เข้าใจเฉพาะClient กับเครื่อง Server เท่านั้น นิยมใช้กับเว็บไซต์ที่ต้องการความปลอดภัยสูง เช่น เว็บไซต์ของธนาคาร ร้านค้าออนไลน์ เป็นต้น</p>
 
 
 
+<p></p>
+
+<a href="http://mindphp.com/%E0%B8%84%E0%B8%B9%E0%B9%88%E0%B8%A1%E0%B8%B7%E0%B8%AD/73-%E0%B8%84%E0%B8%B7%E0%B8%AD%E0%B8%AD%E0%B8%B0%E0%B9%84%E0%B8%A3/2046-http-https-%E0%B8%84%E0%B8%B7%E0%B8%AD%E0%B8%AD%E0%B8%B0%E0%B9%84%E0%B8%A3.html">แหล่งอ้างอิง </a>
+		
 </body>
 </html>
-</div>
-</div>
-</div>
-</div>
-</div>
-@endsection
