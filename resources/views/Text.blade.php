b4375f2a0ec31f12840130da4778473526f617cb
nut4214 <nut4214@hotmail.com>
2018-11-26 11:37:58 +0700

    edit header text view

--- resources/views/Text.blade.php
+++ resources/views/Text.blade.php
@@ -6,7 +6,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Protocol</div>
+                <div class="card-header">Http</div>
 
                 <div class="card-body">
                     @if (session('status'))
